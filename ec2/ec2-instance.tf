82abd3a20281d5e202c168785206538307799954
Somasundaram A P <sundarams73@gmail.com>
2023-03-11 21:26:04 +0530

    removed ec2 tf file to replace it with main

--- ec2/ec2-instance.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  shared_credentials_files = "[C:/Users/newuser/.aws/credentials]"
-}
-
-resource "aws_instance" "test" {
-  ami = "ami-0dfcb1ef8550277af"
-  instance_type     = "t2.micro"
-}
